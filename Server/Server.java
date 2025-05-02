d3cbd634ce001d10deae28eb0413a005b58c34ea
Chaitanya Datar <datar.chaitanya@gmail.com>
2025-05-02 22:31:08 +0530

    Update Server.java

--- Server/Server.java
+++ Server/Server.java
@@ -10,17 +10,16 @@ public class Server{
             Socket client1 = serverSocket.accept();
             System.out.println("Client 1 connected: " + client1.getInetAddress());
 
-            Socket client2 = serverSocket.accept();
-            System.out.println("Client 2 connected: " + client2.getInetAddress());
-
             BufferedReader reader1 = new BufferedReader(new InputStreamReader(client1.getInputStream()));
-            BufferedReader reader2 = new BufferedReader(new InputStreamReader(client2.getInputStream()));
 
             PrintWriter writer1 = new PrintWriter(client1.getOutputStream(), true);
-            PrintWriter writer2 = new PrintWriter(client2.getOutputStream(), true);
 
             writer1.println("You are connected to the server!");
-            writer2.println("You are connected to the server!");
+
+            String line;
+            while(((line = reader1.readLine()) != null)){
+                System.out.println("client1: "+line);
+            }
             
         }
         catch(IOException e){
